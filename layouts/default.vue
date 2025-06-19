8b4595806f28e43bfdacd44e8d628e29733edf06
ucup <nayuya24@gmail.com>
2025-06-19 08:50:58 +0700

    first up

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>My App</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <router-view />
+        <slot />
+      </v-container>
+    </v-main>
+    <slot />
+    <v-footer app color="primary" dark>
+      <span>&copy; 2023 My App</span>
+    </v-footer>
+  </v-app>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  css: [
+    // "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+});
